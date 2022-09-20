4a54ad3b1095ed766dca4d7fe4f99e2ec1ceceae
K93MSY <masaya93@ogaki-tv.ne.jp>
2022-09-20 19:53:38 +0900

    メインの動作の作成をしました　現状はランダムモードだけ実装されています

--- press/assets/quizpress.js
+++ press/assets/quizpress.js
@@ -1,7 +1,29 @@
 function random_select(json){
     let jsonlength = Object.keys(json).length;
-    var f = Math.floor( Math.random() * (jsonlength + 1 - 0) ) + 0 ;
+    var f = Math.floor( Math.random() * (jsonlength) );
     return json[f];
+}
+function main(json){
+    let data = random_select(json.quests);
+    $('#mondai').html(data.q);
+    $('#check').on('click', function() {
+        if($('#kaitoran').val()==data.ans){
+            $('#kaitoran').val("");
+            data = random_select(json.quests);
+            $('#mondai').html(data.q);
+        }
+    });
+    $("body").keypress(function(e) {
+        if (e.keyCode == 13) {
+          // ここに処理を記述
+          if($('#kaitoran').val()==data.ans){
+            $('#kaitoran').val("");
+            data = random_select(json.quests);
+            $('#mondai').html(data.q);
+        }
+          return false;
+        }
+    });
 }
 
 $(function(){
@@ -10,8 +32,7 @@ $(function(){
     $.getJSON("./quest/example.json")
     .done(function (json) {
         // 成功
-        let data = random_select(json);
-        data.q
+        main(json);
     }).fail(function () {
         // 失敗
         alert("エラーが発生しました　再読み込みを行います")
